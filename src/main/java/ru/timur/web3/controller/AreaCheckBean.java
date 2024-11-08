af709f5c6a928cbb8398f5cf1468acf754cf3478
Timur Stupin <hache03@gmail.com>
2024-11-08 23:58:14 +0300

    change graph

--- src/main/java/ru/timur/web3/controller/AreaCheckBean.java
+++ src/main/java/ru/timur/web3/controller/AreaCheckBean.java
@@ -27,48 +27,9 @@ public class AreaCheckBean implements Serializable {
         double x = pointBean.getX();
         double y = pointBean.getY();
         double r = pointBean.getR();
-        double firstAreaA = Math.abs(x) / (r / 7) - 3;
-        double firstSecondAreaA = Math.abs(y / (r / 7) + (double) 3 / 7 * Math.sqrt(33));
-        double firstSecondAreaB = Math.pow((y / (r / 7)) / 3, 2);
-        double firstSecondAreaC = Math.sqrt(Math.abs(firstSecondAreaA) / firstSecondAreaA);
-        boolean firstArea = (y / (r / 7)) >= 0 && Math.pow(x / r, 2)
-                * Math.sqrt(Math.abs(firstAreaA) / (firstAreaA))
-                + firstSecondAreaB
-                * firstSecondAreaC
-                - 1 <= 0;
 
-        double secondAreaA = Math.abs(x) / (r / 7) - 4;
-        boolean secondArea = (y / (r / 7)) < 0 && Math.pow(x / r, 2)
-                * Math.sqrt(Math.abs(secondAreaA) / (secondAreaA))
-                + firstSecondAreaB
-                * firstSecondAreaC
-                - 1 <= 0;
-
-        boolean thirdArea = (y / (r / 7)) < 0 && Math.abs((x / (r / 7)) / 2)
-                - (3 * Math.sqrt(33) - 7) * Math.pow((x / (r / 7)), 2) / 112
-                - 3 + Math.sqrt(1 - Math.pow(Math.abs(Math.abs(x) / (r / 7) - 2) - 1, 2))
-                - y / (r / 7) <= 0;
-
-        boolean fourthArea = Math.abs(x) / (r / 7) <= 1 && Math.abs(x) / (r / 7) >= 0.75
-                && y / (r / 7) <= 3 && y / (r / 7) >= 0
-                && 9 - 8 * Math.abs(x) / (r / 7) >= y / (r / 7);
-
-        boolean fifthArea = y / (r / 7) >= 0
-                && Math.abs(x) / (r / 7) <= 0.75 && Math.abs(x) / (r / 7) >= 0.5
-                && 3 * Math.abs(x) / (r / 7) + 0.75 >= y / (r / 7);
-
-        boolean sixthArea = x / (r / 7) <= 0.5 && x / (r / 7) >= -0.5
-                && y / (r / 7) >= 0
-                && y / (r / 7) <= 2.25;
-
-        double seventhAreaA = Math.abs(x) / (r / 7) - 1;
-        boolean seventhArea = y / (r / 7) >= 0 && 6 * Math.sqrt(10) / 7
-                + (1.5 - 0.5 * Math.abs(x) / (r / 7))
-                * Math.sqrt(Math.abs(seventhAreaA) / seventhAreaA)
-                - 6 * Math.sqrt(10) / 14
-                * Math.sqrt(4 - Math.pow(seventhAreaA, 2)) >= y / (r / 7);
-
-        return firstArea || secondArea || thirdArea
-                || fourthArea || fifthArea || sixthArea || seventhArea;
+        return x * x + y * y <= r * r && y >= 0 && x >= 0 ||
+                y <= 2 * x + r && y >= 0 && x <= 0 ||
+                -r <= x && x <= 0 && -r / 2 <= y && y <= 0;
     }
 }
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -30,45 +30,35 @@ function draw_point(x, y, hit) {
 }
 
 function draw_batman(r) {
-    let k = 49 / r
     calculator.setExpression({
-        id: 'k',
-        latex: 'k=' + k.toString()
+        id: 'area1',
+        latex: `x^{2}+y^{2}<=${r}^2\\{y>=0\\}\\{x>=0\\}`,
+        color: Desmos.Colors.BLUE
     });
     calculator.setExpression({
-        id: 's',
-        latex: '\\s(x)=\\sqrt{(\\abs(x*k/7))/(x*k/7)}',
-        hidden: true
+        id: 'area2',
+        latex: `y<=2x+${r}\\{y>=0\\}\\{x<=0\\}`,
+        color: Desmos.Colors.BLUE
     });
     calculator.setExpression({
-        id: 'graph2',
-        latex: '(x*k/49)^2*s(\\abs(x*k/7)-3)+(y*k/21)^2*s(y*k/7+3*\\sqrt{33}/7)-1=0',
-        color: 'purple'
+        id: 'area3',
+        latex: `-${r}<=x<=0\\{-${r}/2<=y<=0\\}`,
+        color: Desmos.Colors.BLUE
     });
     calculator.setExpression({
-        id: 'graph3',
-        latex: '\\abs(x*k/14)-((3*\\sqrt{33}-7)/112)*(x*k/7)^2-3+\\sqrt{1-(\\abs(\\abs(x*k/7)-2)-1)^2}-y*k/7=0',
-        color: 'purple'
+        id: 'line1',
+        latex: `y=0\\{-${r}<=x<=-${r}/2\\}`,
+        color: Desmos.Colors.BLUE
     });
     calculator.setExpression({
-        id: 'graph4',
-        latex: '9*s((1-\\abs(x*k/7))*(\\abs(x*k/7)-.75))-8*\\abs(x*k/7)-y*k/7=0',
-        color: 'purple'
+        id: 'line2',
+        latex: `y=-${r}/2\\{-${r}<=x<=0\\}`,
+        color: Desmos.Colors.BLUE
     });
     calculator.setExpression({
-        id: 'graph5',
-        latex: '3*\\abs(x*k/7)+.75*s((.75-\\abs(x*k/7))*(\\abs(x*k/7)-.5))-y*k/7=0',
-        color: 'purple'
-    });
-    calculator.setExpression({
-        id: 'graph6',
-        latex: '2.25*s((.5-x*k/7)*(x*k/7+.5))-y*k/7=0',
-        color: 'purple'
-    });
-    calculator.setExpression({
-        id: 'graph7',
-        latex: '6*\\sqrt{10}/7+(1.5-.5*\\abs(x*k/7))*s(\\abs(x*k/7)-1)-6*\\sqrt{10}/14*\\sqrt{4-(\\abs(x*k/7)-1)^2}-y*k/7=0',
-        color: 'purple'
+        id: 'line3',
+        latex: `y=0\\{0<=x<=${r}\\}`,
+        color: Desmos.Colors.BLUE
     });
 
     calculator.setMathBounds({
