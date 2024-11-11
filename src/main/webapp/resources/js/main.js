73893724a9af7d83c72f4ffd4339a902cab50cc7
Timur Stupin <hache03@gmail.com>
2024-11-12 00:40:24 +0300

    animation fixes

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,8 +1,11 @@
 window.addEventListener("load", load_data);
 
+let IS_ANIMATE = true;
+
 document.getElementById('calculator').addEventListener('click', async function (evt) {
     let {x, y} = get_click_coordinates(evt.clientX, evt.clientY);
     try {
+        IS_ANIMATE = false;
         await checkPoint([{name: 'x', value: x}, {name: 'y', value: y}]);
     } catch (e){
         alert('Сервер недоступен. Пожалуйста, попробуйте позже');
@@ -10,11 +13,16 @@ document.getElementById('calculator').addEventListener('click', async function (
 });
 
 function display_point(x, y, hit){
+    if(IS_ANIMATE) completeAnimation();
+    IS_ANIMATE = true;
+    draw_point(x, y, hit);
+}
+
+function completeAnimation(){
     const animatedDiv = document.getElementById('main_form');
     animatedDiv.classList.add('animate');
     animatedDiv.addEventListener('animationend', function() {
         animatedDiv.classList.remove('animate');
-        draw_point(x, y, hit);
     }, { once: true });
 }
 
